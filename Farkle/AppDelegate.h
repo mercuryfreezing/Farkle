0110ce70b10e10a0790e0289fa605831215151d7
roshan <roshan.prakash.r@gmail.com>
2014-10-08 20:05:46 +0100

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by roshan on 08/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by roshan on 08/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
